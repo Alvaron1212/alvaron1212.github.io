6776eca08a56fefda8de2361b5eb924d79b8a781
Alvaron1212 <bloodyalvaron@gmail.com>
2026-03-25 16:32:03 +0700

    Refactor temperature fetching and calculation logic

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
+//// HTML Data ////
 const TemperatureDisplay = document.getElementById('temperature-display');
-const LatitudeInput = document.getElementById('latitude');
-const LongitudeInput = document.getElementById('longitude');
+const LocationInput = document.getElementById('Input');
 const LocationDisplay = document.getElementById('location-display');
+const UnitInput = document.getElementById('Unit');
 
-function GetTemperature(){
-  let Latitude = Number(LatitudeInput.value);
-  let Longitude = Number(LongitudeInput.value);
-  
-  fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${Latitude}&longitude=${Longitude}&localityLanguage=en`)
-  .then(res => res.json())
-  .then(data => {
-    LocationDisplay.textContent = '-----'
-    
-    setTimeout(() => {
-      LocationDisplay.textContent = data.countryName + ', ' + data.localityInfo.administrative[1].name
-    }, 500)
-  });
-  
-  fetch(`https://api.open-meteo.com/v1/forecast?latitude=${Latitude}&longitude=${Longitude}&hourly=temperature_2m&current=temperature_2m`)
-  .then(res => res.json())
-  .then(data => {
-    TemperatureDisplay.textContent = '~';
-    
-    setTimeout(() => {
-      TemperatureDisplay.textContent = Math.round(data.current.temperature_2m)
-    }, 500);
-  });
+//// Function /////
+function Calc(c, unit){
+  if (unit === 'F') {
+    return (c * 9 / 5) + 32;
+  } else if (unit === 'R') {
+    return (c * 4) / 5;
+  };
+  return c;
 };
+
+async function GetTemperature(){
+  // Data
+  const Unit = UnitInput.value;
+  let Location = LocationInput.value;
+  LocationDisplay.textContent = '~~~~~';
+  TemperatureDisplay.textContent = '~';
+  
+  // Fetch
+  try {
+    let GeoAPI = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${Location}`);
+    let GeoData = await GeoAPI.json();
+    
+    if (!GeoData.results){
+      LocationDisplay.textContent = 'Unknown';
+      return
+    }
+    
+    const Place = GeoData.results[0];
+    
+    let TempAPI = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${Place.latitude}&longitude=${Place.longitude}&hourly=temperature_2m&current=temperature_2m`);
+    let TempData = await TempAPI.json();
+    
+    const Temp = TempData.current.temperature_2m;
+    const TempFinal = Math.round(Calc(Temp, Unit));
+    
+    // Displayer
+    TemperatureDisplay.textContent = TempFinal;
+    
+    const PlaceList = [
+      Place.country,
+      Place.admin1,
+      Place.admin2
+    ].filter(Boolean);
+    
+    LocationDisplay.textContent = PlaceList.join(', ');
+  } catch(err){
+    console.log(`Error: ${err}`)
+    LocationDisplay.textContent = 'Error!'
+  }
+}
+
+UnitInput.addEventListener('change', () => {
+  GetTemperature();
+})
